a25442bae1776b0ebcda2c5df643907593226792
salaminus <salaminus@gmail.com>
2025-03-12 12:01:29 +0300

    New BD

--- student_testing/student_testing/settings.py
+++ student_testing/student_testing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4#nqg6zxo+7hkliv@j8h04lde3^i6z$&8ub@l^yf5jsz4()us3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
